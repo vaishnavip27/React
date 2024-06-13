696d77f4866f93cacd6a8b3dff17fdec5972785c
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-06-14 02:40:20 +0530

    component

---
+++ component/src/App.js
@@ -0,0 +1,14 @@
+import MyButton from "./components/MyButton";
+import "./styles/MyButton.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">Welcome to my App</h1>
+      <MyButton />
+      <p>Hello there, this is Vaishnavi</p>
+    </div>
+  );
+}
+
+export default App;
