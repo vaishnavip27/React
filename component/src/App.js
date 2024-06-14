a2d04f79bc62f1355e1291f3b2f9792976225363
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-06-15 00:55:06 +0530

    profile and styling elements

--- component/src/App.js
+++ component/src/App.js
@@ -1,12 +1,21 @@
 import MyButton from "./components/MyButton";
 import "./styles/MyButton.css";
+import image from "./images/photo-1.jpg";
+import UserGreeting from "./UserGreeting";
 
 function App() {
+  const user = {
+    name: "Vaishnavi Patil",
+    imageSize: 90,
+  };
+
   return (
     <div>
       <h1 className="heading">Welcome to my App</h1>
       <MyButton />
       <p>Hello there, this is Vaishnavi</p>
+      <img src={image} alt="profile-image" class="photo-1" />
+      <UserGreeting isLoggedIn={true} username="Vaishnavi patil" />
     </div>
   );
 }
